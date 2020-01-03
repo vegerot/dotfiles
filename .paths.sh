0239952a6b6260c3bf0d58b98738ce80c02e5f00
Max Coplan <mchcopl@yahoo.com>
2020-01-03 18:42:43 -0500

    More efficient vim loading

--- .paths.sh
+++ .paths.sh
@@ -1,7 +1,7 @@
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/lsof/bin:$PATH:/usr/local/opt:/opt:/Users/maxcoplan/bin:/usr/local/anaconda3/bin:/usr/local/sbin"
 export PATH="$PATH:$HOME/Library/Python/3.7/bin:/usr/local/lib/python3.7/site-packages"
 export PATH="$PATH:/usr/local/share"
-export PATH="$PATH:$HOME/.gem/ruby/2.6.0/bin"
+export PATH="/usr/local/opt/ruby/bin:$PATH:/usr/local/lib/ruby/gems/2.6.0/bin:$HOME/.gem/ruby/2.6.0/bin"
 export PATH="$HOME/.jenv/bin:$PATH"
 export PATH="$PATH:$HOME/workspace/spark-2.4.4-bin-hadoop2.7/bin"
 
