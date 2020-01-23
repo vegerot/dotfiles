a10bf0a12475f6787f43007da54db8ba70f9cac6
Max Coplan <mchcopl@yahoo.com>
2020-01-22 20:01:28 -0500

    added iterm settings

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,22 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 set -o xtrace
-pwd
-function deploy() {
+
+function doIt() {
     if [ "$1" "==" "--dry-run" ]; then
-        find . -type f -not -path "*/.git/*" -not -path "./.DS_Store" -not -path "./.osx" -not -path "./bootstrap.sh" -not -path "./brew.sh" -not -path "./README.md" -not -path "./LICENSE-MIT.txt" -exec echo ~/dotfiles/'{}' "->" ~/'{}' \;
+        find . -type f -not -path "*/.git/*" -not -path "./.DS_Store" -not -path "./.osx" -not -path "./bootstrap.sh" -not -path "./brew.sh" -not -path "*/node_modules/*" -not -path "./README.md" -not -path "./LICENSE-MIT.txt" -exec echo ~/dotfiles/'{}' "->" ~/'{}' \;
     elif [ "$1" "==" "--force" ]; then
-        find . -type f -not -path "*/.git/*" -not -path "./.DS_Store" -not -path "./.osx" -not -path "./bootstrap.sh" -not -path "./README.md" -not -path "./LICENSE-MIT.txt" -exec mkdir -p $(dirname {}) \; -exec ln -fsv ~/dotfiles/'{}' ~/'{}' \;
+	    find . -type f -not -path "*/.git/*" -not -path "./init" -not -path "./brew.sh" -not -path "./.DS_Store" -not -path "./.macos" -not -path "./bootstrap.sh" -not -path "*/node_modules/*" -not -path "./README.md" -not -path "./LICENSE-MIT.txt" -exec sh -c 'file="{}"; mkdir -pv $HOME/${file%/*}/; ln -fsv ~/dotfiles/$file ~/$file' \;
     else
         echo "NOTE: This will not overwrite any existing files.  Rerun with --force to overwrite existing dotfiles"
-        find . -type f -not -path "*/.git/*" -not -path "./.DS_Store" -not -path "./.osx" -not -path "./bootstrap.sh" -not -path "./README.md" -not -path "./LICENSE-MIT.txt" -exec mkdir -p $(dirname {}) \; -exec ln -sv ~/dotfiles/'{}' ~/'{}' \;
+        find . -type f -not -path "*/.git/*" -not -path "./.DS_Store" -not -path "./.osx" -not -path "./bootstrap.sh" -not -path "*/node_modules/*" -not -path "./README.md" -not -path "./LICENSE-MIT.txt" -exec mkdir -pv $(dirname {}) \; -exec ln -sv ~/dotfiles/'{}' ~/'{}' \;
     fi
     set +x
-	source ~/.zshrc;
+	source ~/.bash_profile;
 }
 
 if [[ "$1" == "--force" ]]; then
-	doIt;
+	doIt --force;
 else
 	read "REPLY?This may overwrite existing files in your home directory. Are you sure? (y/n) ";
 	echo "";
