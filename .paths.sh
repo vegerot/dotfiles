a41cd117fcdebaed8a53b37f543186001a258250
Max 👨🏽‍💻 Coplan <Max.Coplan@walmart.com>
2021-09-20 10:37:50 -0400

    🔧 Make vim slow 🐢

--- .paths.sh
+++ .paths.sh
@@ -1,12 +1,11 @@
 #Path stuff
 
-export PATH="$PATH:$HOME/go/bin:/usr/local/lib"
-
-# make sure this is the last thing
-export PATH="$PATH:."
-
+export PATH="$PATH:$HOME/go/bin:/usr/local/lib:$HOME/dotfiles/bin"
 
 #sledge:binary path
 export SLEDGE_BIN=/Users/m0c0j7y/.sledge/bin
 export PATH="${PATH}:${SLEDGE_BIN}"
+
+# make sure this is the last thing
+export PATH="$PATH:."
 
