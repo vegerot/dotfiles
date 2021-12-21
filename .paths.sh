5855763c9486ea97464779e5aa76793d3d0df8d5
Max 👨🏽‍💻 Coplan <Max.Coplan@walmart.com>
2021-12-21 15:09:28 -0800

    🎨 Update aliases

--- .paths.sh
+++ .paths.sh
@@ -1,11 +1,11 @@
 #Path stuff
 
 ## important stuff goes first
-
+export PATH="/usr/local/opt/ruby/bin:$PATH"
 
 
 ## Unimportant stuff goes at the end
-export PATH="$PATH:/usr/local/lib:$HOME/go/bin:/opt/cisco/anyconnect/bin:$HOME/dotfiles/bin"
+export PATH="$PATH:/usr/local/lib:$HOME/go/bin:/opt/cisco/anyconnect/bin:$HOME/dotfiles/bin:$HOME/.mint/bin"
 
 # sledge:binary path
 export SLEDGE_BIN=/Users/m0c0j7y/.sledge/bin
