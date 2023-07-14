231a34aa0a5affb13adf7266c170da2490d8d7fe
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2023-07-13 20:38:56 -0700

    nvim: format coqConfig

--- .config/nvim/lua/coqConfig.lua
+++ .config/nvim/lua/coqConfig.lua
@@ -1,14 +1,13 @@
 vim.g.coq_settings = {
-  ["clients.tabnine"] = {
-    enabled = true,
-    weight_adjust = -0.4,
-  },
-  auto_start = 'shut-up',
-  -- conflicts with Tmux
-  ["keymap.jump_to_mark"] = ''
+	["clients.tabnine"] = {
+		enabled = true,
+		weight_adjust = -0.4,
+	},
+	auto_start = 'shut-up',
+	-- conflicts with Tmux
+	["keymap.jump_to_mark"] = ''
 }
 
 require("coq_3p") {
-	{src = "copilot", short_name="✈", accept_key="<c-f>"}
-
+	{ src = "copilot", short_name = "✈", accept_key = "<c-f>" }
 }
