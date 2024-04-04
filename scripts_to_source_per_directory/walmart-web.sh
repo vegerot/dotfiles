13794475c0d1a621ad2d6c3668915aaa0c2c8420
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2024-04-04 08:48:01 -0700

    perf(walmart): nvm is way faster!

--- scripts_to_source_per_directory/walmart-web.sh
+++ scripts_to_source_per_directory/walmart-web.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # symlink me in `walmart-web/max_scripts_source_on_cd.sh`
 
@@ -35,13 +35,53 @@ function uninstrument_for_profiling() {
 	done
 }
 
-function setUpNvmIfNotExists() {
+function debug() {
+	if [[ -z $QUIET ]]; then return; fi
+	echo "$@"
+}
+
+function fast_nvm_use() {
 	local has_nvmrc_file=$([[ -f .nvmrc ]]&& true || false)
 	# this comes from ~/.sh_functions
 	local has_nvmuse=$(command -v nvmuse) && true || false
 
 	if [[ $has_nvmrc == false || $has_nvmuse == false ]]; then
-		echo "not setting up nvm because requirement is missing"
+		debug "not setting up fast nvm because requirement is missing"
+		return
+	fi
+	export NVM_DIR="$HOME/.nvm/"
+	local nvm_versions_dir="$NVM_DIR/versions/node/"
+	# TODO: search upwards for `.nvmrc`
+	local node_version="$(cat .nvmrc)"
+	local node_path="$nvm_versions_dir/$node_version"
+
+	if [[ ! -d "$node_path" ]]; then
+		debug "not setting up fast nvm because can't find the requested node version"
+		debug "doing slow version instead 🐢"
+		set +x
+		export NVM_DIR="$HOME/.nvm"
+		[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+		[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+		nvm use $@
+		return 1
+	fi
+
+	export PATH="$node_path/bin:$PATH"
+	export MANPATH="$node_path/share/man:$MANPATH"
+	set +x
+}
+
+function nvmuse() {
+	fast_nvm_use "$@"
+}
+
+function setUpNvmIfNotSetUp() {
+	local has_nvmrc_file=$([[ -f .nvmrc ]]&& true || false)
+	# this comes from ~/.sh_functions
+	local has_nvmuse=$(command -v nvmuse) && true || false
+
+	if [[ $has_nvmrc == false || $has_nvmuse == false ]]; then
+		debug "not setting up nvm because requirement is missing"
 		return
 	fi
 
@@ -49,7 +89,9 @@ function setUpNvmIfNotExists() {
 	local want_version=$(cat .nvmrc)
 
 	if [[ $got_version != $want_version ]]; then
+		debug "changing PATH to Node $want_version from Node $got_version"
 		nvmuse
+		return
 	fi
 }
 
@@ -72,16 +114,16 @@ function setUpNvmIfNotExists() {
 #```
 
 function node_nvm_wrapper() {
-	setUpNvmIfNotExists
-	command $*
+	setUpNvmIfNotSetUp;
+	command $*;
 }
 
 function node() {
-	node_nvm_wrapper $0 $*
+	node_nvm_wrapper $0 $*;
 }
 
 function pnpm() {
-	node_nvm_wrapper $0 $*
+	node_nvm_wrapper $0 $*;
 }
 
 alias gc="setUpNvmIfNotExists && git commit"
