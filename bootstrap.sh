f105185851d3c943110b0da82395e3679006b2ae
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2024-05-11 16:36:10 -0700

    fix(bootstrap): correctly make directories

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ function doIt() {
         find . -type f -not -path "*/.git/*" -not -path "./.DS_Store" -not -path "./.osx" -not -path "./bootstrap.sh" -not -path "./brew.sh" -not -path "./README.md" -not -path "./LICENSE-MIT.txt" -exec echo ~/dotfiles/'{}' "->" ~/'{}' \;
     elif [ "$1" "==" "--force" ]; then
         echo "forced"
-        find . -type f -not -path "*/.git/*" -not -path "./.DS_Store" -not -path "./.osx" -not -path "./bootstrap.sh" -not -path "./README.md" -not -path "./LICENSE-MIT.txt" -exec mkdir -p $(dirname {}) \; -exec ln --force --symbolic --verbose ~/dotfiles/'{}' ~/'{}' \;
+        find . -type f -not -path "*/.git/*" -not -path "./.DS_Store" -not -path "./.osx" -not -path "./bootstrap.sh" -not -path "./README.md" -not -path "./LICENSE-MIT.txt" | xargs -I{} bash -xc 'cd $HOME; mkdir -pv $(dirname {}) ; ln --symbolic --force --verbose ~/dotfiles/{} ~/{};'
     else
         echo "NOTE: This will not overwrite any existing files.  Rerun with --force to overwrite existing dotfiles"
         find . -type f -not -path "*/.git/*" -not -path "./.DS_Store" -not -path "./.osx" -not -path "./bootstrap.sh" -not -path "./README.md" -not -path "./LICENSE-MIT.txt" | xargs -I{} bash -xc 'cd $HOME; mkdir -pv $(dirname {}) ; ln --symbolic --verbose ~/dotfiles/{} ~/{};'
