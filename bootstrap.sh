a287a17389c6afd50afc23d707fc44a95c0d20b9
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2024-05-11 16:15:09 -0700

    fix(bootstrap): make cowardly version work more cowardly

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,23 +5,21 @@ set -o xtrace
 function doIt() {
     if [ "$1" "==" "--dry-run" ]; then
         find . -type f -not -path "*/.git/*" -not -path "./.DS_Store" -not -path "./.osx" -not -path "./bootstrap.sh" -not -path "./brew.sh" -not -path "./README.md" -not -path "./LICENSE-MIT.txt" -exec echo ~/dotfiles/'{}' "->" ~/'{}' \;
-    #elif [ "$1" "==" "--force" ]; then
-    else
+    elif [ "$1" "==" "--force" ]; then
         echo "forced"
-        find . -type f -not -path "*/.git/*" -not -path "./.DS_Store" -not -path "./.osx" -not -path "./bootstrap.sh" -not -path "./README.md" -not -path "./LICENSE-MIT.txt" -exec mkdir -p $(dirname {}) \; -exec ln -fsv ~/dotfiles/'{}' ~/'{}' \;
-    #else
+        find . -type f -not -path "*/.git/*" -not -path "./.DS_Store" -not -path "./.osx" -not -path "./bootstrap.sh" -not -path "./README.md" -not -path "./LICENSE-MIT.txt" -exec mkdir -p $(dirname {}) \; -exec ln --force --symbolic --verbose ~/dotfiles/'{}' ~/'{}' \;
+    else
         echo "NOTE: This will not overwrite any existing files.  Rerun with --force to overwrite existing dotfiles"
-        find . -type f -not -path "*/.git/*" -not -path "./.DS_Store" -not -path "./.osx" -not -path "./bootstrap.sh" -not -path "*/node_modules/*" -not -path "./README.md" -not -path "./LICENSE-MIT.txt" -exec mkdir -pv $(dirname {}) \; -exec ln -sv ~/dotfiles/'{}' ~/'{}' \;
+        find . -type f -not -path "*/.git/*" -not -path "./.DS_Store" -not -path "./.osx" -not -path "./bootstrap.sh" -not -path "./README.md" -not -path "./LICENSE-MIT.txt" | xargs -I{} bash -xc 'cd $HOME; mkdir -pv $(dirname {}) ; ln --symbolic --verbose ~/dotfiles/{} ~/{};'
     fi
     set +x
 	#source ~/.zshrc;
 }
 
 if [[ "$1" == "--force" ]]; then
-	doIt --force;
+		doIt $1;
 else
 	read "REPLY?This may overwrite existing files in your home directory. Are you sure? (y/n) ";
-	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt $1;
 	fi;
