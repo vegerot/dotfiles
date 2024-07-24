2f36f89fe505c00e0deb0554f2fd5e49c1c80134
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2024-07-24 15:58:54 -0700

    refactor: delete unused config files

--- .compFlags.sh
+++
@@ -1,3 +0,0 @@
-export LDFLAGS="-L/usr/local/opt/ruby/lib $LDFLAGS"
-export CPPFLAGS="-I/usr/local/opt/ruby/include $CPPFLAGS"
-export PKG_CONFIG_PATH="/usr/local/opt/ruby/lib/pkgconfig $PKG_CONFIG_PATH"
