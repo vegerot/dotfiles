c9851e76be00fcaeea046293e57abfecc0b224f4
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2024-12-13 17:24:18 -0800

    fix(tiktok): set email in tiktok repos

--- scripts_to_source_per_directory/tiktok-go.sh
+++ scripts_to_source_per_directory/tiktok-go.sh
@@ -2,3 +2,11 @@
 export GOPROXY="https://go-mod-proxy.byted.org,https://proxy.golang.org,direct"
 export GOPRIVATE="*.byted.org,*.everphoto.cn"
 export GOSUMDB="sum.golang.google.cn"
+
+function set_tiktok_email() {
+        if [[ -d .git && ! $(grep -e "^email =" .git/sl/config 2>/dev/null) ]]; then
+                sl config --local ui.email=Max.Coplan@Bytedance.com
+        fi
+}
+
+set_tiktok_email
