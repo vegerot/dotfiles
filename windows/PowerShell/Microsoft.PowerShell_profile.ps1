58f00fb46287b9de76619611361ddd5aadd61d08
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2024-12-31 21:28:36 -0800

    feat(windows): add powershell config

---
+++ windows/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,19 @@
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+# prompt
+function prompt {
+  $loc = $executionContext.SessionState.Path.CurrentLocation;
+
+  $out = "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
+  if ($loc.Provider.Name -eq "FileSystem") {
+    $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+  }
+  return $out
+}
+
+Set-Alias sap sl.exe
+
+#oh-my-posh init pwsh | Invoke-Expression
